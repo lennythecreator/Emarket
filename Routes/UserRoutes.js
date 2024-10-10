788245985492969541506304df2db6a6c2f41116
Godsheritage  Adeoye <Adeoyegodsheritage@gmail.com>
2024-10-09 21:22:06 -0400

    udpates

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -6,4 +6,8 @@ userRouter.get('/api/v1/user/:uid', (req, res) => {
     res.status(200).json("Route Successful")
 })
 
+userRouter.get('/api/v1/users', (req, res) => {
+    res.status(200).json("Route Successful")
+})
+
 module.exports = userRouter
